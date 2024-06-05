07f3854c96d0461010fae098fb36a4295b04c812
vignesh5oct <vignesh5oct@gmail.com>
2024-06-05 17:07:46 +0530

    6th commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,7 @@ import HomePage from './pages/homePage/HomePage';
 const Home = () => {
 
   const Bg = styled.div`
-  width: 1600px;
-  height: 1800px;
+  
   background: #F5F5F5 0% 0% no-repeat padding-box;
   opacity: 1;
   `;
